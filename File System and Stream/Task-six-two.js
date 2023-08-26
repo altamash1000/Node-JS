a73628e969c5164d092a631b6226edc5444f070b
Altamash Asif Ghori <139010361+altamash1000@users.noreply.github.com>
2023-08-27 00:45:37 +0500

    Delete File System and Stream directory

--- File System and Stream/Task-six-two.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const writeStream = fs.createWriteStream('file-six.txt', 'utf8');
-
-writeStream.write('Data to be written.\n');
-
-writeStream.end(() => {
-    console.log('Writing finished.');
-});
